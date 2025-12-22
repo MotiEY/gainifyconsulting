973d9a4898e525976d968cb8e770be7f28f29d9b
MotiEY <motiisl@yahoo.com>
2025-12-22 20:52:37 +0200

    Update App.tsx

--- App.tsx
+++ App.tsx
@@ -1,9 +1,8 @@
+// /App.tsx
 import React from 'react';
 import Header from './components/Header';
 import Hero from './components/Hero';
 import Features from './components/Features';
-import RealityDemo from './components/RealityDemo';
-import Tracks from './components/Tracks';
 import Methodology from './components/Methodology';
 import PulseTool from './components/PulseTool';
 import Results from './components/Results';
@@ -14,42 +13,14 @@ const App: React.FC = () => {
   return (
     <div className="bg-white text-slate-900">
       <Header />
-
       <main>
-        {/* Anchor target for header/footer logo */}
-        <section id="top">
-          <Hero />
-        </section>
-
-        <section id="layers">
-          <Features />
-        </section>
-
-        <section id="reality-demo">
-          <RealityDemo />
-        </section>
-
-        <section id="tracks">
-          <Tracks />
-        </section>
-
-        <section id="how-we-work">
-          <Methodology />
-        </section>
-
-        <section id="pulse">
-          <PulseTool />
-        </section>
-
-        <section id="results">
-          <Results />
-        </section>
-
-        <section id="contact">
-          <Contact />
-        </section>
+        <Hero />
+        <Features />
+        <Methodology />
+        <PulseTool />
+        <Results />
+        <Contact />
       </main>
-
       <Footer />
     </div>
   );
